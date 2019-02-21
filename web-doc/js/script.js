502875e84587bbff4cff7f67f5aef2b2ccb3924c
kokolinche <43370185+kokolinche@users.noreply.github.com>
2019-02-21 11:08:10 +0100

    Add files via upload

---
+++ web-doc/js/script.js
@@ -0,0 +1,32 @@
+function PlaySound(soundobj) {
+    var sound1 = document.getElementById("sound1");
+    sound1.play();
+}
+
+function StopSound(soundobj) {
+    var sound1 = document.getElementById("sound1");
+    sound1.pause();
+    sound1.currentTime = 0;
+}
+
+function PlaySound2(soundobj) {
+    var sound2 = document.getElementById("sound2");
+    sound2.play();
+}
+
+function StopSound2(soundobj) {
+    var sound2 = document.getElementById("sound2");
+    sound2.pause();
+    sound2.currentTime = 0;
+}
+
+function PlaySound3(soundobj) {
+    var sound3 = document.getElementById("sound3");
+    sound3.play();
+}
+
+function StopSound3(soundobj) {
+    var sound3 = document.getElementById("sound3");
+    sound3.pause();
+    sound3.currentTime = 0;
+}
